46122f66fbe5762447e46beeab54b352e44851e7
unknown <youu11ef@gmail.com>
2023-09-20 13:56:28 +0300

    first commit

---
+++ codes.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+
+# I will share the code soon, and for those who need it right now, it is available in the report, but only screenshots.
+# please note that the report was first written in french, then translated. Some words are still in French in the code screenshots but will
+# be translated as well when it will be posted. Thanks !
